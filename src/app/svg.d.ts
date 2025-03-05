7b5384db98ea9fe76469e98cf730cb0aad8fd070
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2025-03-05 10:22:31 +0200

    fix: correct SVG type

--- src/app/svg.d.ts
+++ src/app/svg.d.ts
@@ -1,7 +1,5 @@
 declare module '*.svg' {
-  import React from 'react';
-
-  const content: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const content: string;
 
   export default content;
 }
