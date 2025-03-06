f70a4aa96f0b1d8b4a8705ff816710494e6eb6e0
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2025-03-06 11:25:13 +0200

    NotFoundError on 404

--- src/app/not-found/page.tsx
+++ src/app/not-found/page.tsx
@@ -1,8 +1,22 @@
+'use client';
 import Link from 'next/link';
+import { useEffect } from 'react';
+
+import ActiveBugsnag from '../services/bugsnag';
 
 import styles from './page.module.css';
 
+class NotFoundError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'NotFoundError';
+  }
+}
+
 export default function NotFound() {
+  useEffect(() => {
+    ActiveBugsnag.notify(new NotFoundError(globalThis.location.pathname));
+  }, []);
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>404</h1>
