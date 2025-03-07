b4e347ad596a34a77bbfc38b0542565bd1b0d00c
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2025-03-07 15:28:45 +0200

    fix test

--- src/shared/schemas/indexation-table.test.ts
+++ src/shared/schemas/indexation-table.test.ts
@@ -5,6 +5,7 @@ import { indexationTableSchema } from './indexation-table';
 describe('indexationTableSchema', () => {
   it('should validate a correct indexation table', () => {
     const validTable = {
+      date: '2024-09-17',
       id: 1,
       tableFilename: 'valid_filename.csv',
       location: [50.4501, 30.5234],
@@ -30,6 +31,7 @@ describe('indexationTableSchema', () => {
 
   it('should invalidate a table with invalid id', () => {
     const invalidTable = {
+      date: '2024-09-17',
       id: 0,
       tableFilename: 'valid_filename.csv',
       location: [50.4501, 30.5234],
@@ -43,6 +45,7 @@ describe('indexationTableSchema', () => {
 
   it('should invalidate a table with invalid size', () => {
     const invalidTable = {
+      date: '2024-09-17',
       id: 1,
       tableFilename: 'valid_filename.csv',
       location: [50.4501, 30.5234],
@@ -56,6 +59,7 @@ describe('indexationTableSchema', () => {
 
   it('should invalidate a table with invalid locale', () => {
     const invalidTable = {
+      date: '2024-09-17',
       id: 1,
       tableFilename: 'valid_filename.csv',
       location: [50.4501, 30.5234],
@@ -63,19 +67,6 @@ describe('indexationTableSchema', () => {
       sources: ['source1', 'source2'],
       title: 'Valid Title',
       tableLocale: 'en',
-    };
-    expect(() => indexationTableSchema.parse(invalidTable)).toThrow();
-  });
-
-  it('should invalidate a table with an empty source', () => {
-    const invalidTable = {
-      id: 1,
-      tableFilename: 'valid_filename.csv',
-      location: [50.4501, 30.5234],
-      size: 100,
-      sources: [''],
-      title: 'Valid Title',
-      tableLocale: 'uk',
     };
     expect(() => indexationTableSchema.parse(invalidTable)).toThrow();
   });
