c54562f62d451242aa10ee80a20d67207d459aca
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2025-03-12 18:03:42 +0200

    limit batch size

--- src/populate-typesense/import-batch.ts
+++ src/populate-typesense/import-batch.ts
@@ -1,46 +1,36 @@
 import _ from 'lodash';
-import type { HTTPError } from 'typesense/lib/Typesense/Errors';
 
 import calculatePayloadSizeInBytes from './calculate-payload-size-in-bytes';
 import type { RowForImport } from './types';
 import typesense from './typesense';
 
+const BATCH_SIZE_LIMIT = 1_000_000;
+
 export default async function importBatch(
   collectionName: string,
   batch: RowForImport[],
 ) {
+  const batchSizeInBytes = calculatePayloadSizeInBytes(batch);
   console.log(
-    `importBatch for a batch of ${batch.length} weighing ${calculatePayloadSizeInBytes(batch)}`,
+    `importBatch for a batch of ${batch.length} weighing ${batchSizeInBytes}`,
   );
-  try {
-    await typesense
-      .collections(collectionName)
-      .documents()
-      .import(batch, { action: 'upsert' });
-    return batch.length;
-  } catch (error) {
+  if (batchSizeInBytes > BATCH_SIZE_LIMIT) {
     let processedSize = 0;
-    console.error(error);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
-    console.log((error as any)['name']);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
-    console.log((error as any)['httpBody']);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any
-    console.log(Object.keys(error as any));
-    if ((error as HTTPError).httpStatus === 413) {
-      console.log(`Batch too big: ${batch.length}. Splitting it in halves.`);
-      const chunks = _.chunk(batch, Math.trunc(batch.length / 2));
-      for (const chunk of chunks) {
-        processedSize += await importBatch(collectionName, chunk);
-      }
-      if (processedSize !== batch.length) {
-        throw new Error(
-          `${batch.length - processedSize} of ${batch.length} batch records were lost somehow.`,
-        );
-      }
-      return processedSize;
-    } else {
-      throw error;
+    console.log(`Batch too big: ${batch.length}. Splitting it in halves.`);
+    const chunks = _.chunk(batch, Math.trunc(batch.length / 2));
+    for (const chunk of chunks) {
+      processedSize += await importBatch(collectionName, chunk);
     }
+    if (processedSize !== batch.length) {
+      throw new Error(
+        `${batch.length - processedSize} of ${batch.length} batch records were lost somehow.`,
+      );
+    }
+    return processedSize;
   }
+  await typesense
+    .collections(collectionName)
+    .documents()
+    .import(batch, { action: 'upsert' });
+  return batch.length;
 }
