67a38a9c12e98be9f55a81a4cf8c54c97ff10b03
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2025-03-12 12:43:55 +0200

    fix indexation table tests

--- src/shared/schemas/indexation-table.test.ts
+++ src/shared/schemas/indexation-table.test.ts
@@ -13,6 +13,7 @@ describe('indexationTableSchema', () => {
       sources: ['source1', 'source2'],
       title: 'Valid Title',
       tableLocale: 'uk',
+      yearsRange: [1822, 1853],
     };
     expect(() => indexationTableSchema.parse(validTable)).not.toThrow();
   });
@@ -25,6 +26,7 @@ describe('indexationTableSchema', () => {
       size: 100,
       sources: ['source1', 'source2'],
       title: 'Valid Title',
+      yearsRange: [1822, 1853],
     };
     expect(() => indexationTableSchema.parse(invalidTable)).toThrow();
   });
@@ -39,6 +41,7 @@ describe('indexationTableSchema', () => {
       sources: ['source1', 'source2'],
       title: 'Valid Title',
       tableLocale: 'uk',
+      yearsRange: [1822, 1853],
     };
     expect(() => indexationTableSchema.parse(invalidTable)).toThrow();
   });
@@ -53,6 +56,7 @@ describe('indexationTableSchema', () => {
       sources: ['source1', 'source2'],
       title: 'Valid Title',
       tableLocale: 'uk',
+      yearsRange: [1822, 1853],
     };
     expect(() => indexationTableSchema.parse(invalidTable)).toThrow();
   });
@@ -67,6 +71,51 @@ describe('indexationTableSchema', () => {
       sources: ['source1', 'source2'],
       title: 'Valid Title',
       tableLocale: 'en',
+      yearsRange: [1822, 1853],
+    };
+    expect(() => indexationTableSchema.parse(invalidTable)).toThrow();
+  });
+
+  it('should invalidate a table with missing yearsRange', () => {
+    const invalidTable = {
+      date: '2024-09-17',
+      id: 'valid-id',
+      tableFilename: 'valid_filename.csv',
+      location: [50.4501, 30.5234],
+      size: 100,
+      sources: ['source1', 'source2'],
+      title: 'Valid Title',
+      tableLocale: 'uk',
+    };
+    expect(() => indexationTableSchema.parse(invalidTable)).toThrow();
+  });
+
+  it('should invalidate a table with empty yearsRange', () => {
+    const invalidTable = {
+      date: '2024-09-17',
+      id: 'valid-id',
+      tableFilename: 'valid_filename.csv',
+      location: [50.4501, 30.5234],
+      size: 100,
+      sources: ['source1', 'source2'],
+      title: 'Valid Title',
+      tableLocale: 'uk',
+      yearsRange: [],
+    };
+    expect(() => indexationTableSchema.parse(invalidTable)).toThrow();
+  });
+
+  it('should invalidate a table with big yearsRange', () => {
+    const invalidTable = {
+      date: '2024-09-17',
+      id: 'valid-id',
+      tableFilename: 'valid_filename.csv',
+      location: [50.4501, 30.5234],
+      size: 100,
+      sources: ['source1', 'source2'],
+      title: 'Valid Title',
+      tableLocale: 'uk',
+      yearsRange: [1822, 1823, 1824],
     };
     expect(() => indexationTableSchema.parse(invalidTable)).toThrow();
   });
