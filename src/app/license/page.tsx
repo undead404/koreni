fd86c8c042a223f192d204d365279cd092c5d61b
alinkedd <alina.listunova@gmail.com>
2025-03-14 13:16:04 +0100

    Convert license to html

--- src/app/license/page.tsx
+++ src/app/license/page.tsx
@@ -1,31 +1,35 @@
 import { readFile } from 'node:fs/promises';
 
+import { remark } from 'remark';
+import html from 'remark-html';
+
 import styles from './page.module.css';
 
 export default async function LicensePage() {
   const licenseMarkdownBuffer = await readFile('./LICENSE.md');
-  const licenseMarkdown = licenseMarkdownBuffer.toString();
+  const licenseHTMLVFile = await remark()
+    .use(html)
+    .process(licenseMarkdownBuffer);
+  const licenseHTML = licenseHTMLVFile.toString();
 
   return (
     <article className={styles.root}>
       <h1>Ліцензія</h1>
-      <section>
-        <p>
-          Вміст Коренів заснований на праці багатьох осіб, вказаних на сторінках
-          відповідних таблиць. Ці особи передали ці таблиці Кореням для
-          поширення за ліцензією{' '}
-          <a href="https://opendatacommons.org/licenses/odbl/">ODbL</a>. Повний
-          текст ліцензії наведено нижче.
-        </p>
-        <p>
-          Самі дані можна отримати{' '}
-          <a href="https://github.com/undead404/koreni/tree/main/data">
-            у директорії <code>data</code> на GitHub Коренів
-          </a>
-          .
-        </p>
-        <pre dangerouslySetInnerHTML={{ __html: licenseMarkdown }}></pre>
-      </section>
+      <p>
+        Вміст Коренів заснований на праці багатьох осіб, вказаних на сторінках
+        відповідних таблиць. Ці особи передали ці таблиці Кореням для поширення
+        за ліцензією{' '}
+        <a href="https://opendatacommons.org/licenses/odbl/">ODbL</a>. Повний
+        текст ліцензії наведено нижче.
+      </p>
+      <p>
+        Самі дані можна отримати{' '}
+        <a href="https://github.com/undead404/koreni/tree/main/data">
+          у директорії <code>data</code> на GitHub Коренів
+        </a>
+        .
+      </p>
+      <section dangerouslySetInnerHTML={{ __html: licenseHTML }} />
     </article>
   );
 }
