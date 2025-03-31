ea1a6e585a5e4cb2d2a9b0364b674cbf5a5de2c4
alinkedd <alina.listunova@gmail.com>
2025-03-31 23:22:07 +0200

    Rework pagination

--- src/app/components/pagination.tsx
+++ src/app/components/pagination.tsx
@@ -1,3 +1,5 @@
+import PaginationButton from './pagination-button';
+
 import styles from './pagination.module.css';
 
 export default function Pagination({
@@ -12,25 +14,111 @@ export default function Pagination({
   if (totalPages <= 1) {
     return null;
   }
+
+  if (totalPages <= 7) {
+    return (
+      <nav className={styles.nav} aria-labelledby="table-pagination">
+        <h3 id="table-pagination" className="visually-hidden">
+          Пагінація
+        </h3>
+        <ul className={`no-disc ${styles.ul}`}>
+          {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+            (page) => (
+              <li>
+                <PaginationButton
+                  key={page}
+                  page={page}
+                  isCurrent={currentPage === page}
+                  urlBuilder={urlBuilder}
+                />
+              </li>
+            ),
+          )}
+        </ul>
+      </nav>
+    );
+  }
+
+  const isStarting = currentPage <= 4;
+  const isEnding = currentPage >= totalPages - 3;
+
   return (
     <nav className={styles.nav} aria-labelledby="table-pagination">
       <h3 id="table-pagination" className="visually-hidden">
         Пагінація
       </h3>
       <ul className={`no-disc ${styles.ul}`}>
-        {Array.from({ length: totalPages }, (_, index) => index + 1).map(
-          (page) => (
-            <li key={page} className={styles.li}>
-              {currentPage === page ? (
-                <span className={styles.currentPage}>{page}</span>
-              ) : (
-                <a href={urlBuilder(page)} className={styles.link}>
-                  {page}
-                </a>
-              )}
-            </li>
-          ),
+        <li>
+          <PaginationButton
+            page={1}
+            isCurrent={currentPage === 1}
+            urlBuilder={urlBuilder}
+          />
+        </li>
+
+        {isStarting ? (
+          <li>
+            <PaginationButton
+              page={2}
+              isCurrent={currentPage === 2}
+              urlBuilder={urlBuilder}
+            />
+          </li>
+        ) : (
+          <li>
+            <span className={styles.rest}>...</span>
+          </li>
         )}
+
+        <li>
+          <PaginationButton
+            page={isStarting ? 3 : isEnding ? totalPages - 4 : currentPage - 1}
+            isCurrent={currentPage === 3}
+            urlBuilder={urlBuilder}
+          />
+        </li>
+        <li>
+          <PaginationButton
+            page={isStarting ? 4 : isEnding ? totalPages - 3 : currentPage}
+            isCurrent={
+              isStarting
+                ? currentPage === 4
+                : isEnding
+                  ? currentPage === totalPages - 3
+                  : true
+            }
+            urlBuilder={urlBuilder}
+          />
+        </li>
+        <li>
+          <PaginationButton
+            page={isStarting ? 5 : isEnding ? totalPages - 2 : currentPage + 1}
+            isCurrent={currentPage === totalPages - 2}
+            urlBuilder={urlBuilder}
+          />
+        </li>
+
+        {isEnding ? (
+          <li>
+            <PaginationButton
+              page={totalPages - 1}
+              isCurrent={currentPage === totalPages - 1}
+              urlBuilder={urlBuilder}
+            />
+          </li>
+        ) : (
+          <li>
+            <span className={styles.rest}>...</span>
+          </li>
+        )}
+
+        <li>
+          <PaginationButton
+            page={totalPages}
+            isCurrent={currentPage === totalPages}
+            urlBuilder={urlBuilder}
+          />
+        </li>
       </ul>
     </nav>
   );
