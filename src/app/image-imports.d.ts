73f1b11006aaef03a84421b93c47015e72fcb4aa
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2025-11-29 12:08:26 +0200

    fix image imports

---
+++ src/app/image-imports.d.ts
@@ -0,0 +1,23 @@
+declare module '*.png' {
+  import type { StaticImageData } from 'next/image';
+  const value: StaticImageData;
+  export default value;
+}
+
+declare module '*.jpg' {
+  import type { StaticImageData } from 'next/image';
+  const value: StaticImageData;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  import type { StaticImageData } from 'next/image';
+  const value: StaticImageData;
+  export default value;
+}
+
+declare module '*.svg' {
+  import type { StaticImageData } from 'next/image';
+  const value: StaticImageData;
+  export default value;
+}
