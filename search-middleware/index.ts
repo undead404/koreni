b9d69606d27f74e0d4326c734f6c68e0d42fddae
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2026-02-09 10:08:17 +0200

    delete redundant search-middleware

--- search-middleware/index.ts
+++
@@ -1,3 +0,0 @@
-import start from './start';
-
-void start();
--- search-middleware/notify-queue-positions.ts
+++
@@ -1,7 +0,0 @@
-import requestQueue from './queue';
-
-export default function notifyQueuePositions() {
-  for (const [index, item] of requestQueue.entries()) {
-    item.conn.send(JSON.stringify({ position: index + 1 }));
-  }
-}
