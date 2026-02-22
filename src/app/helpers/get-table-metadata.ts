4edab2475e99e88934654694a4d6d5b84a8c05e4
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2026-02-22 23:02:52 +0200

    fix: register-unaware slugs

--- src/app/helpers/get-table-metadata.ts
+++ src/app/helpers/get-table-metadata.ts
@@ -3,15 +3,25 @@ import path from 'node:path';
 
 import { parse } from 'yaml';
 
+import getYamlFilepaths from '@/shared/get-yaml-filepaths';
 import {
   type IndexationTable,
   indexationTableSchema,
 } from '@/shared/schemas/indexation-table';
+const METADATA_FOLDER = path.join(process.cwd(), 'data/records');
+
+const yamlFilepaths = await getYamlFilepaths(METADATA_FOLDER);
 
 export default async function getTableMetadata(
   tableId: string,
 ): Promise<IndexationTable> {
-  const yamlFilepath = path.join(process.cwd(), `data/records/${tableId}.yaml`);
+  const lowercaseTableId = tableId.toLowerCase();
+  const yamlFilepath = yamlFilepaths.find((filepath) =>
+    filepath.toLowerCase().endsWith(`${lowercaseTableId}.yaml`),
+  );
+  if (!yamlFilepath) {
+    throw new Error('Table not found');
+  }
   const fileContent = await readFile(yamlFilepath, 'utf8');
   return indexationTableSchema.parse(parse(fileContent));
 }
