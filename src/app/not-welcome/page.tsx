457f65dd0f055d6f9302051613ea71eebdbef0c8
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2026-05-18 17:55:20 +0300

    fix: remove fix

--- src/app/not-welcome/page.tsx
+++ src/app/not-welcome/page.tsx
@@ -1,10 +1,15 @@
+'use client';
 import Image from 'next/image';
+import { useEffect } from 'react';
 
 import happyKremlin from '../assets/happykremlin.webp';
 
 import styles from './page.module.css';
 
 export default function NotWelcomePage() {
+  useEffect(() => {
+    document.querySelector('#blukach-widget-host')?.remove();
+  }, []);
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>You are not welcome here</h1>
